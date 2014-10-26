6652a10ee459eae532611d55c317ebfa7deaa5ea
wingon.niu <y-g.niu@163.com>
2014-10-27 00:46:21 +0800

    edit ceph install part 2

--- DeployScripts/Ceph-Install/ceph-install-part-2.sh
+++ DeployScripts/Ceph-Install/ceph-install-part-2.sh
@@ -11,7 +11,21 @@ for HOST_NAME in $(cat ./conf/ceph-server-hostname-info.txt | awk '{print $3}');
     ./check-ssh-connect.expect $HOST_NAME
 done
 
-#
+#Begin ceph deploy
+CURRENT_DIR=$(pwd)
+mkdir -p /root/my-ceph-cluster
+cd /root/my-ceph-cluster
+pwd
+
+#Create the cluster
+CEPH_MON_NODES=$($CURRENT_DIR/gen-server-info-list.sh $CURRENT_DIR/conf/ceph-mon-ip-hostname-info.txt "")
+echo "Run command: ceph-deploy new $CEPH_MON_NODES"
+################## ceph-deploy new $CEPH_MON_NODES
+
+#Change the default number of replicas in the Ceph configuration file from 3 to 2
+#here
+
+cd $CURRENT_DIR
 
 #
 
