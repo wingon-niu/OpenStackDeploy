b4b7e83f68e3366a8ff8bd2f54b7ca33d2c35197
wingon.niu <y-g.niu@163.com>
2014-10-27 17:43:36 +0800

    edit ceph install part 2

--- DeployScripts/Ceph-Install/ceph-install-part-2.sh
+++ DeployScripts/Ceph-Install/ceph-install-part-2.sh
@@ -23,7 +23,24 @@ echo "Run command: ceph-deploy new $CEPH_MON_NODES"
 ################## ceph-deploy new $CEPH_MON_NODES
 
 #Change the default number of replicas in the Ceph configuration file from 3 to 2
+/root/OpenStack-Install-HA/set-config.py ./ceph.conf global osd_pool_default_size 2
+
+#Add the ceph public and cluster network setting
+CEPH_PUBLIC_NETWORK_IP=$(head  -n 1 $CURRENT_DIR/conf/ceph-mon-ip-hostname-info.txt | awk '{print $1}')
+CEPH_CLUSTER_NETWORK_IP=$(head -n 1 $CURRENT_DIR/conf/ceph-mon-ip-hostname-info.txt | awk '{print $2}')
+CEPH_PUB_NET=${CEPH_PUBLIC_NETWORK_IP%.*}.0/24
+CEPH_CLT_NET=${CEPH_CLUSTER_NETWORK_IP%.*}.0/24
+echo "Ceph public  network : $CEPH_PUBLIC_NETWORK_IP   $CEPH_PUB_NET"
+echo "Ceph cluster network : $CEPH_CLUSTER_NETWORK_IP  $CEPH_CLT_NET"
+/root/OpenStack-Install-HA/set-config.py     ./ceph.conf global public_network  $CEPH_PUB_NET
+if [ "$CEPH_PUB_NET" != "$CEPH_CLT_NET" ]; then
+    /root/OpenStack-Install-HA/set-config.py ./ceph.conf global cluster_network $CEPH_CLT_NET
+fi
+
+#Install Ceph
 #here
+
+#
 
 cd $CURRENT_DIR
 
