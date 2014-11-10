94e983434922d01378d20e6024bbe8bd5ba2adb9
wingon.niu <y-g.niu@163.com>
2014-11-10 23:11:15 +0800

    edit horizon install use juno release

--- DeployScripts/OpenStack-Install-HA/horizon-install.sh
+++ DeployScripts/OpenStack-Install-HA/horizon-install.sh
@@ -6,7 +6,7 @@ echo "Begin time of horizon-install:"
 date
 
 #Install horizon
-apt-get install -y apache2 memcached libapache2-mod-wsgi openstack-dashboard
+apt-get install -y openstack-dashboard apache2 libapache2-mod-wsgi memcached python-memcache
 apt-get remove -y --purge openstack-dashboard-ubuntu-theme
 
 #Modify /etc/openstack-dashboard/local_settings.py
@@ -14,6 +14,7 @@ CONF_FILE=/etc/openstack-dashboard/local_settings.py
 ./backup-file.sh $CONF_FILE
 
 sed -i "/^OPENSTACK_HOST/c OPENSTACK_HOST = \"$KEYSTONE_EXT_HOST_IP\"" $CONF_FILE
+sed -i "/^ALLOWED_HOSTS/c  ALLOWED_HOSTS = ['*']"                      $CONF_FILE
 
 NUM=$(cat $CONF_FILE | grep ':11211' | wc -l)
 if [ $NUM -eq 0 ]; then
