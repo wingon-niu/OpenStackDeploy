13d7510ac722f7a9d659243d0f10a841a83a9ee9
wingon.niu <y-g.niu@163.com>
2014-11-20 17:52:02 +0800

    edit openstack undeploy scripts

--- DeployScripts/OpenStack-Install-HA/compute-node-uninstall.sh
+++ DeployScripts/OpenStack-Install-HA/compute-node-uninstall.sh
@@ -7,4 +7,23 @@ echo "Now running compute node uninstall on $MY_HOST_NAME"
 
 #
 
+./stop-service-nova-compute.sh
+./stop-service-nova-network.sh
+./stop-service-compute-node-neutron.sh
+service openvswitch-switch stop
+
+#
+
+apt-get -y     remove openvswitch-switch neutron-common neutron-plugin-ml2 neutron-plugin-openvswitch-agent
+apt-get -y autoremove openvswitch-switch neutron-common neutron-plugin-ml2 neutron-plugin-openvswitch-agent --purge
+
+#
+
+rm -rf /var/log/neutron
+rm -rf /var/lib/neutron
+rm -rf /etc/neutron
+rm -rf /var/log/openvswitch
+
+#
+
 exit 0
