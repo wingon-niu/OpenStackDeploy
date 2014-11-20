6e9e0ff83d3b759653172b2fc64547c75b62427e
wingon.niu <y-g.niu@163.com>
2014-11-20 22:38:17 +0800

    edit openstack undeploy scripts

---
+++ DeployScripts/OpenStack-Install-HA/uninstall-all-openstack-components.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+
+ovs-vsctl del-br br-int
+ovs-vsctl del-br br-tun
+ovs-vsctl del-br br-ex
+
+#
+
+./stop-service-keystone.sh
+./stop-service-glance.sh
+./stop-service-cinder.sh
+./stop-service-horizon.sh
+./stop-service-nova-api.sh
+./stop-service-nova-compute.sh
+./stop-service-nova-network.sh
+./stop-service-controller-node-neutron.sh
+./stop-service-network-node-neutron.sh
+./stop-service-compute-node-neutron.sh
+service openvswitch-switch stop
+
+#
+
+apt-get -y remove \
+           keystone python-keystoneclient \
+           glance python-glanceclient \
+           cinder-api cinder-scheduler python-cinderclient lvm2 cinder-volume \
+           openstack-dashboard apache2 libapache2-mod-wsgi memcached python-memcache \
+           nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy nova-scheduler python-novaclient \
+           nova-compute sysfsutils \
+           nova-network nova-api-metadata \
+           neutron-common neutron-plugin-ml2 neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent \
+           neutron-server python-neutronclient openvswitch-switch
+
+apt-get -y autoremove \
+           keystone python-keystoneclient \
+           glance python-glanceclient \
+           cinder-api cinder-scheduler python-cinderclient lvm2 cinder-volume \
+           openstack-dashboard apache2 libapache2-mod-wsgi memcached python-memcache \
+           nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy nova-scheduler python-novaclient \
+           nova-compute sysfsutils \
+           nova-network nova-api-metadata \
+           neutron-common neutron-plugin-ml2 neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent \
+           neutron-server python-neutronclient openvswitch-switch \
+           --purge
+
+#
+
+rm -rf /etc/keystone
+rm -rf /etc/glance
+rm -rf /etc/cinder
+rm -rf /etc/openstack-dashboard
+rm  -f /etc/memcached.conf
+rm -rf /etc/nova
+rm -rf /etc/neutron
+
+#
+
+exit 0
