ed7193ef7adc617630d53f2bd1e41cd1711df626
wingon.niu <y-g.niu@163.com>
2014-11-21 17:53:56 +0800

    edit controller node uninstall scripts

--- DeployScripts/OpenStack-Install-HA/controller-node-uninstall.sh
+++ DeployScripts/OpenStack-Install-HA/controller-node-uninstall.sh
@@ -8,10 +8,23 @@ echo "Now running controller node uninstall on $MY_HOST_NAME"
 #
 
 ./uninstall-all-openstack-components.sh
+./cinder-volumes-remove.sh
 
 #Uninstall RabbitMQ
+./stop-service-rabbitmq.sh
+apt-get -y remove     rabbitmq-server
+apt-get -y autoremove rabbitmq-server --purge
+rm -rf /var/lib/rabbitmq
 
 #Uninstall Mysql Galera
+./stop-service-mysql.sh
+dpkg -r galera
+dpkg -r mysql-server-wsrep
+dpkg -P galera
+dpkg -P mysql-server-wsrep
+rm  -rf /var/lib/mysql
+cp /etc/mysql/my.cnf.bak.100           /etc/mysql/my.cnf
+cp /etc/mysql/conf.d/wsrep.cnf.bak.100 /etc/mysql/conf.d/wsrep.cnf
 
 #
 
