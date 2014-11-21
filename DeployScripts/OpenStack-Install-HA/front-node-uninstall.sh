779e528c70d97067ff6c2ba98204689aec4fce9e
wingon.niu <y-g.niu@163.com>
2014-11-21 23:00:20 +0800

    edit front node uninstall scripts

--- DeployScripts/OpenStack-Install-HA/front-node-uninstall.sh
+++ DeployScripts/OpenStack-Install-HA/front-node-uninstall.sh
@@ -7,4 +7,12 @@ echo "Now running front node uninstall on $MY_HOST_NAME"
 
 #
 
+/etc/init.d/keepalived stop
+/etc/init.d/haproxy    stop
+
+apt-get -y remove     haproxy keepalived
+apt-get -y autoremove haproxy keepalived --purge
+
+#
+
 exit 0
