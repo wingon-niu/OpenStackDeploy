5a25ec2308da2f408cfec372500de36dfc4d51d7
wingon.niu <y-g.niu@163.com>
2014-11-24 21:21:46 +0800

    do not remove lvm2

--- DeployScripts/OpenStack-Install-HA/uninstall-all-openstack-components.sh
+++ DeployScripts/OpenStack-Install-HA/uninstall-all-openstack-components.sh
@@ -25,24 +25,24 @@ service openvswitch-switch stop
 apt-get -y remove \
            keystone python-keystoneclient \
            glance python-glanceclient \
-           cinder-api cinder-scheduler python-cinderclient lvm2 cinder-volume \
+           cinder-api cinder-scheduler python-cinderclient cinder-volume \
            openstack-dashboard apache2 libapache2-mod-wsgi memcached python-memcache \
            nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy nova-scheduler python-novaclient \
-           nova-compute sysfsutils \
+           nova-compute \
            nova-network nova-api-metadata \
            neutron-common neutron-plugin-ml2 neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent \
-           neutron-server python-neutronclient openvswitch-switch
+           neutron-server python-neutronclient
 
 apt-get -y autoremove \
            keystone python-keystoneclient \
            glance python-glanceclient \
-           cinder-api cinder-scheduler python-cinderclient lvm2 cinder-volume \
+           cinder-api cinder-scheduler python-cinderclient cinder-volume \
            openstack-dashboard apache2 libapache2-mod-wsgi memcached python-memcache \
            nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy nova-scheduler python-novaclient \
-           nova-compute sysfsutils \
+           nova-compute \
            nova-network nova-api-metadata \
            neutron-common neutron-plugin-ml2 neutron-plugin-openvswitch-agent neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent \
-           neutron-server python-neutronclient openvswitch-switch \
+           neutron-server python-neutronclient \
            --purge
 
 #
