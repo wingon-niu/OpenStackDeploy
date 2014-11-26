f55244ee924029bf2cae3c1cd217c0d0dac83a15
wingon.niu <y-g.niu@163.com>
2014-11-26 16:30:11 +0800

    add openstack services functional scripts

--- DeployScripts/OpenStack-Install-HA/network-node-restart-all-openstack-services.sh
+++ DeployScripts/OpenStack-Install-HA/network-node-restart-all-openstack-services.sh
@@ -2,11 +2,18 @@
 
 source ./env.sh
 
-
+#
 
 if [ "$NETWORK_API_CLASS" = "neutron" ]; then
+
+    ./restart-service-network-node-neutron.sh 2>&1 | awk '{printf "    %-36s  %-17s  %-10s  %-10s\n",$1,$2,$3,$4}'
+
 elif [ "$NETWORK_API_CLASS" = "nova-network" ]; then
+
+    ./restart-service-nova-network.sh         2>&1 | awk '{printf "    %-36s  %-17s  %-10s  %-10s\n",$1,$2,$3,$4}'
+
 else
+    DO_NOTHING="do_nothing"
 fi
 
 #
--- DeployScripts/OpenStack-Install-HA/network-node-show-all-openstack-services-status.sh
+++ DeployScripts/OpenStack-Install-HA/network-node-show-all-openstack-services-status.sh
@@ -2,11 +2,18 @@
 
 source ./env.sh
 
-
+#
 
 if [ "$NETWORK_API_CLASS" = "neutron" ]; then
+
+    ./show-service-status-network-node-neutron.sh 2>&1 | awk '{printf "    %-36s  %-17s  %-10s  %-10s\n",$1,$2,$3,$4}'
+
 elif [ "$NETWORK_API_CLASS" = "nova-network" ]; then
+
+    ./show-service-status-nova-network.sh         2>&1 | awk '{printf "    %-36s  %-17s  %-10s  %-10s\n",$1,$2,$3,$4}'
+
 else
+    DO_NOTHING="do_nothing"
 fi
 
 #
