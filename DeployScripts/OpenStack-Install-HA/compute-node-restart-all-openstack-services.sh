e4782c559a78806b8b39b4f56bfdcba9080aa42b
wingon.niu <y-g.niu@163.com>
2014-11-26 16:53:46 +0800

    add openstack services functional scripts

--- DeployScripts/OpenStack-Install-HA/compute-node-restart-all-openstack-services.sh
+++ DeployScripts/OpenStack-Install-HA/compute-node-restart-all-openstack-services.sh
@@ -2,12 +2,19 @@
 
 source ./env.sh
 
-
+#
 
 if [ "$NETWORK_API_CLASS" = "neutron" ]; then
+
+    ./restart-service-compute-node-neutron.sh 2>&1 | awk '{printf "    %-36s  %-17s  %-10s  %-10s\n",$1,$2,$3,$4}'
+
 elif [ "$NETWORK_API_CLASS" = "nova-network" ]; then
+    DO_NOTHING="do_nothing"
 else
+    DO_NOTHING="do_nothing"
 fi
+
+./restart-service-nova-compute.sh             2>&1 | awk '{printf "    %-36s  %-17s  %-10s  %-10s\n",$1,$2,$3,$4}'
 
 #
 
