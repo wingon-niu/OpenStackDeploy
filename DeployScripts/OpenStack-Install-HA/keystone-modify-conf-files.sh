9e47f8fec17c03e1a2cf6640ecb3b700a9971e89
wingon.niu <y-g.niu@163.com>
2014-11-28 16:21:23 +0800

    edit keystone modify conf files

--- DeployScripts/OpenStack-Install-HA/keystone-modify-conf-files.sh
+++ DeployScripts/OpenStack-Install-HA/keystone-modify-conf-files.sh
@@ -5,12 +5,14 @@ source ./env.sh
 CONF_FILE=/etc/keystone/keystone.conf
 ./backup-file.sh $CONF_FILE
 
-./set-config.py $CONF_FILE DEFAULT  admin_token  $KEYSTONE_ADMIN_TOKEN
-./set-config.py $CONF_FILE DEFAULT  admin_port   35357
-./set-config.py $CONF_FILE DEFAULT  verbose      True
-./set-config.py $CONF_FILE database connection   mysql://$KEYSTONE_USER:$KEYSTONE_PASS@$DATABASE_IP/keystone
-./set-config.py $CONF_FILE token    provider     keystone.token.providers.uuid.Provider
-./set-config.py $CONF_FILE token    driver       keystone.token.persistence.backends.sql.Token
+./set-config.py $CONF_FILE DEFAULT  admin_token       $KEYSTONE_ADMIN_TOKEN
+./set-config.py $CONF_FILE DEFAULT  admin_port        35357
+./set-config.py $CONF_FILE DEFAULT  verbose           True
+./set-config.py $CONF_FILE database connection        mysql://$KEYSTONE_USER:$KEYSTONE_PASS@$DATABASE_IP/keystone
+./set-config.py $CONF_FILE database idle_timeout      60
+./set-config.py $CONF_FILE token    provider          keystone.token.providers.uuid.Provider
+./set-config.py $CONF_FILE token    driver            keystone.token.persistence.backends.sql.Token
+./set-config.py $CONF_FILE cache    memcache_servers  $CONTROLLER_NODE_MANAGEMENT_IP:11211
 
 #./set-config.py $CONF_FILE signing  token_format UUID
 
