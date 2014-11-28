8a20589b14b16c17eadfe41354bab0d8a1fc15c3
wingon.niu <y-g.niu@163.com>
2014-11-28 23:47:11 +0800

    select openstack and ceph release

--- DeployScripts/Ceph-Install/ceph-install-part-1.sh
+++ DeployScripts/Ceph-Install/ceph-install-part-1.sh
@@ -4,9 +4,14 @@ HOST_NAME=$(hostname)
 echo "Now running ceph-install-part-1.sh on $HOST_NAME"
 
 apt-get -y install wget expect
+
 wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | apt-key add -
-rm -f /etc/apt/sources.list.d/ceph.list
-echo "deb http://ceph.com/debian-firefly/ trusty main" | tee /etc/apt/sources.list.d/ceph.list
+
+rm -f          /etc/apt/sources.list.d/ceph.list
+cp ./ceph.list /etc/apt/sources.list.d/
+
+###echo "deb http://ceph.com/debian-firefly/ trusty main" | tee /etc/apt/sources.list.d/ceph.list
+
 apt-get -y update
 apt-get -y install ceph-deploy
 
--- DeployScripts/OpenStack-Install-HA/node-3-preparing-openstack.sh
+++ DeployScripts/OpenStack-Install-HA/node-3-preparing-openstack.sh
@@ -3,13 +3,19 @@
 source ./env.sh
 
 apt-get install -y ubuntu-cloud-keyring python-software-properties
-echo "deb       http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/juno main"   >  /etc/apt/sources.list.d/cloudarchive-juno.list
-echo "# deb-src http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/juno main"  >>  /etc/apt/sources.list.d/cloudarchive-juno.list
 
-#add-apt-repository cloud-archive:juno
+rm -f               /etc/apt/sources.list.d/openstack.list
+cp ./openstack.list /etc/apt/sources.list.d/
+
+###echo "deb       http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/juno main"   >  /etc/apt/sources.list.d/cloudarchive-juno.list
+###echo "# deb-src http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/juno main"  >>  /etc/apt/sources.list.d/cloudarchive-juno.list
+
+######add-apt-repository cloud-archive:juno
 
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
+
+#
 
 exit 0
