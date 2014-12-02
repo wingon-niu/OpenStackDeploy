00ff668e6c9c9b873ff4fc01bfc7e453b72f9f36
wingon.niu <y-g.niu@163.com>
2014-12-03 00:42:49 +0800

    add virsh secret-undefine in compute node uninstall

--- DeployScripts/OpenStack-Install-HA/compute-node-uninstall.sh
+++ DeployScripts/OpenStack-Install-HA/compute-node-uninstall.sh
@@ -7,6 +7,10 @@ echo "Now running compute node uninstall on $MY_HOST_NAME"
 
 #
 
+if [ -f ./uuid.txt ]; then
+    virsh secret-undefine --secret $(cat ./uuid.txt | awk '{print $1}')
+fi
+
 ./uninstall-all-openstack-components.sh
 
 #
